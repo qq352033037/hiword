2e4519637476a5ee7316a647dc96dc7f70596c72
qq352033037 <352033037@qq.com>
2020-03-01 16:22:46 +0800

    test

--- src/main/java/test/App.java
+++ src/main/java/test/App.java
@@ -8,10 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!888" );
+        System.out.println( "Hello World!888" );
+        System.out.println( "Hello World!888" );
+        System.out.println( "Hello World!888" );
+        System.out.println( "Hello World!888" );
     }
 }
